2f7eb3cb2de72cc057d31963d7cea3702a1db7fe
prasannan_robots <prasannanatarajan3595@gmail.com>
2023-08-03 16:09:59 +0530

    test update

--- test.py
+++ test.py
@@ -3,4 +3,5 @@ from verifier import Email_Verifier
 verifier = Email_Verifier('prasannanatarajan3595@gmail.com')
 print(verifier.check_regex())
 print(verifier.check_regex_rfc())
+print(verifier.check_domain())
 
